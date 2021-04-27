964a2d96b37f282f1a00a8d0e1ebf4a46b09fba4
holiday526 <48387601+holiday526@users.noreply.github.com>
2021-04-27 23:19:20 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,12 +7,10 @@
 
             <div class="col-lg">
 
-                <div class="moving-banner mt-2">
+                <div class="moving-banner mt-2 my-1">
                     <h3 style="color: #92abba">Welcome to {{ \Illuminate\Support\Facades\Config::get('constants.APP_NAME') }} <i class="fas fa-hamburger"></i></h3>
                 </div>
-
                 @if (count($feature_foods) > 0)
-
                 <div>
                     <b-carousel
                         class="mb-4"
@@ -34,26 +32,29 @@
                         @endforeach
                     </b-carousel>
                 </div>
-                <h4 class="my-4">Featured Food</h4>
-                <b-card-group class="my-4" deck>
+                <h4 class="my-4 ml-2"><i class="fas fa-star"></i> Featured Food</h4>
+                <b-row class="my-4">
                     @foreach($feature_foods as $food)
-                        <food-card
-                            @auth
-                            :logged-in="true"
-                            @else
-                            :logged-in="false"
-                            @endauth
-                            food-id="{{$food->food_id}}"
-                            food-name="{{$food->food_name}}"
-                            food-description="{{$food->food_description}}"
-                            food-price="{{$food->food_price}}"
-                            food-type="{{$food->food_type_name}}"
-                            food-category="{{$food->food_category_name}}"
-                            food-image="{{$food->food_image_location}}"
-                        >
-                        </food-card>
+                        <b-col class="mx-auto">
+                            <food-card
+                                class="mx-auto"
+                                @auth
+                                :logged-in="true"
+                                @else
+                                :logged-in="false"
+                                @endauth
+                                food-id="{{$food->food_id}}"
+                                food-name="{{$food->food_name}}"
+                                food-description="{{$food->food_description}}"
+                                food-price="{{$food->food_price}}"
+                                food-type="{{$food->food_type_name}}"
+                                food-category="{{$food->food_category_name}}"
+                                food-image="{{$food->food_image_location}}"
+                            >
+                            </food-card>
+                        </b-col>
                     @endforeach
-                </b-card-group>
+                </b-row>
                 @else
                     <h4>Wait for food menu update</h4>
                 @endif
